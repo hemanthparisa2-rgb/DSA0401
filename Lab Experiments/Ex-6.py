e460d617ee080fd32dd4a5599d49a008c30b0d86
hemanthparisa2-rgb <hemanthparisa2@gmail.com>
2026-09-02 08:24:48 +0530

    Update Ex-6.py

--- Lab Experiments/Ex-6.py
+++ Lab Experiments/Ex-6.py
@@ -1,29 +1,26 @@
 #Customer purchase
 import numpy as np
 # Item prices and quantities
-prices = np.array([50, 100, 30])
-quantities = np.array([2, 1, 4])
+import numpy as np
 
-# Discount and tax rates (in percentage)
+data = np.genfromtxt("customer_purchase.csv", delimiter=",", skip_header=1)
+
+prices = data[:, 1]
+quantities = data[:, 2]
+
 discount_rate = 10
 tax_rate = 5
 
-# Calculate subtotal
 subtotal = np.sum(prices * quantities)
 
-# Calculate discount
 discount = (discount_rate / 100) * subtotal
 
-# Price after discount
 amount_after_discount = subtotal - discount
 
-# Calculate tax
 tax = (tax_rate / 100) * amount_after_discount
 
-# Final total cost
 total_cost = amount_after_discount + tax
 
-# Display results
 print("Subtotal:", subtotal)
 print("Discount:", discount)
 print("Tax:", tax)
