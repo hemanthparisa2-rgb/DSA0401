5633c630cb28d65d7d07415aef93d6d9f65f0fec
hemanthparisa2-rgb <hemanthparisa2@gmail.com>
2026-09-02 08:23:45 +0530

    Update Ex-5.py

--- Lab Experiments/Ex-5.py
+++ Lab Experiments/Ex-5.py
@@ -1,16 +1,14 @@
 #Fuel Efficiency
 import numpy as np
 
-# Fuel efficiency of different car models (in miles per gallon)
-fuel_efficiency = np.array([20, 25, 30, 35])
+data = np.genfromtxt("fuel_efficiency.csv", delimiter=",", skip_header=1, dtype=None, encoding="utf-8")
 
-# Calculate average fuel efficiency
+fuel_efficiency = data[:, 1].astype(float)
+
 average_efficiency = np.mean(fuel_efficiency)
 
-# Calculate percentage improvement from Model 1 to Model 4
-percentage_improvement = ((fuel_efficiency[3] - fuel_efficiency[0]) / fuel_efficiency[0]) * 100
+percentage_improvement = ((fuel_efficiency[-1] - fuel_efficiency[0]) / fuel_efficiency[0]) * 100
 
-# Display results
 print("Fuel Efficiency:", fuel_efficiency)
 print("Average Fuel Efficiency:", average_efficiency)
 print("Percentage Improvement: {:.2f}%".format(percentage_improvement))
