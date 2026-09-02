32eeba240860fa1202af4351ea40d61e7fcfce41
hemanthparisa2-rgb <hemanthparisa2@gmail.com>
2026-09-02 08:26:01 +0530

    Update Ex-8.py

--- Lab Experiments/Ex-8.py
+++ Lab Experiments/Ex-8.py
@@ -1,15 +1,10 @@
 #Analyzing sales past month
 import pandas as pd
-# Sample DataFrame
-sales_data = pd.DataFrame({
-    'Product_Name': ['Laptop', 'Mouse', 'Laptop', 'Keyboard',
-                     'Mouse', 'Laptop', 'Keyboard', 'Monitor'],
-    'Quantity_Sold': [5, 10, 3, 7, 8, 6, 2, 9]
-})
 
-# Find the top 5 products sold the most
+sales_data = pd.read_csv("sales.csv")
+
 top_5_products = (
-    sales_data.groupby('Product_Name')['Quantity_Sold']
+    sales_data.groupby("Product_Name")["Quantity_Sold"]
     .sum()
     .sort_values(ascending=False)
     .head(5)
